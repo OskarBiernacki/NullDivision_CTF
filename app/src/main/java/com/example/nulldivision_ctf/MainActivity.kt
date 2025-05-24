e89b32612e3bfe91ef9396f58c768e5e2aae9d96
05KAr8!3Rn4cK1 <ozifozx@gmail.com>
2025-05-24 13:03:26 +0200

    Login screen & loading

--- app/src/main/java/com/example/nulldivision_ctf/MainActivity.kt
+++ app/src/main/java/com/example/nulldivision_ctf/MainActivity.kt
@@ -4,23 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.example.nulldivision_ctf.ui.theme.NullDivision_CTFTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,27 +38,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NullDivision_CTFTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(modifier = Modifier
-                        .padding(innerPadding)
-                        .fillMaxSize().background(colorResource(id = R.color.background)))
-                }
+                AppContent()
             }
         }
     }
 }
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
+fun AppContent() {
+    var isLoading by remember { mutableStateOf(false) }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        if (!isLoading) {
+            LoginScreen(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.background)),
+                onLoginClicked = { isLoading = true }
+            )
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.background)),
+                contentAlignment = Alignment.Center
+            ) {
+                AnimatedDotsText("Connecting server...", color = colorResource(id = R.color.hackerBlue),
+                    onFinishLoading = {isLoading=false}
+                    )
+            }
+        }
+    }
+}
+@Composable
+fun AnimatedDotsText(baseText: String, color: androidx.compose.ui.graphics.Color, onFinishLoading: () -> Unit) {
+    var dotCount by remember { mutableIntStateOf(0) }
+    LaunchedEffect(Unit) {
+        while (dotCount<10) {
+            dotCount = (dotCount + 1)
+            kotlinx.coroutines.delay(500)
+        }
+        onFinishLoading()
+    }
+
+    val dots = ".".repeat(dotCount%4)
+    Text(text = "$baseText$dots", color = color)
+}
+
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
     var login by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    val logo = painterResource(R.drawable.logo)
+
     Box(
         modifier = modifier,
-        contentAlignment = androidx.compose.ui.Alignment.Center
     ) {
         Column(
-            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+            verticalArrangement = androidx.compose.foundation.layout.Arrangement.Top
         ) {
+            Image(
+                painter = logo,
+                contentDescription = null,
+
+            )
             androidx.compose.material3.OutlinedTextField(
                 value = login,
                 onValueChange = {login=it},
@@ -84,17 +140,33 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             )
             androidx.compose.material3.Button(
                 onClick = {
-                    password = "";
-
-                          },
-                modifier = Modifier.padding(top = 16.dp),
-                colors = androidx.compose.material3.ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.hackerBlue),
-                    contentColor = colorResource(id = R.color.background),
-
-                )
+                    password = ""
+                    onLoginClicked()
+                },
+                border = BorderStroke(
+                    width = 2.dp,
+                    color = colorResource(id = R.color.hackerBlue),
+//                    shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp)
+                ),
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .background(
+                        color = colorResource(id = android.R.color.transparent),
+                        shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp)
+                    ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = android.R.color.transparent),
+                    contentColor = colorResource(id = R.color.hackerBlue)
+                ),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp)
             ) {
-                Text("Login")
+                Text(
+                    text = "LOGIN",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 2.sp
+                )
             }
         }
     }

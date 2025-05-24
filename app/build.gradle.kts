1aae596126404c3c72bc167a87067c2546ab405e
05KAr8!3Rn4cK1 <ozifozx@gmail.com>
2025-05-24 22:04:56 +0200

    Server communication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.android.gms:play-services-cronet:18.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/nulldivision_ctf/MainActivity.kt
+++ app/src/main/java/com/example/nulldivision_ctf/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.nulldivision_ctf
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -24,13 +29,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.example.nulldivision_ctf.ui.theme.NullDivision_CTFTheme
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.sp
+import androidx.core.content.res.TypedArrayUtils.getText
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.coroutines.delay
+import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,15 +52,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NullDivision_CTFTheme {
-                AppContent()
+                AppContent(this)
             }
         }
     }
 }
 
+fun makeAPIRequest(main : ComponentActivity, serverUrl : String, method : String, data : JSONObject, onResponseReaction: (response :  JSONObject ) -> Unit ) {
+    val volleyQueue = Volley.newRequestQueue(main)
+
+//        val jsonBody = JSONObject()
+//        jsonBody.put("login", login)
+//        jsonBody.put("password", password)
+
+    val jsonObjectRequest = JsonObjectRequest(
+        Request.Method.GET,
+        serverUrl,
+        data,
+        { response ->
+            onResponseReaction(response)
+//              response.get("message")
+        },
+        { error ->
+            Toast.makeText(main, error.message, Toast.LENGTH_LONG).show()
+        }
+    )
+    volleyQueue.add(jsonObjectRequest)
+}
+
 @Composable
-fun AppContent() {
+fun AppContent(main : ComponentActivity) {
     var isLoading by remember { mutableStateOf(false) }
+    var loadingMessage by remember { mutableStateOf("") }
+    val url = "http://${stringResource(id = R.string.serverUrl)}:${stringResource(id = R.string.serverPORT)}/"
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         if (!isLoading) {
@@ -55,7 +93,20 @@ fun AppContent() {
                     .padding(innerPadding)
                     .fillMaxSize()
                     .background(colorResource(id = R.color.background)),
-                onLoginClicked = { isLoading = true }
+                onLoginClicked = {login, password ->
+
+                    val jsonBody = JSONObject()
+                    jsonBody.put("login", login)
+                    jsonBody.put("password", password)
+
+                    makeAPIRequest(main, url, "login",jsonBody, { response ->
+                        val message = response.get("token");
+                        Toast.makeText(main, message.toString(), Toast.LENGTH_LONG).show()
+                    })
+
+                    isLoading = true;
+                },
+                loadingMessage
             )
         } else {
             Box(
@@ -65,19 +116,19 @@ fun AppContent() {
                 contentAlignment = Alignment.Center
             ) {
                 AnimatedDotsText("Connecting server...", color = colorResource(id = R.color.hackerBlue),
-                    onFinishLoading = {isLoading=false}
+                    onFinishLoading = {isLoading=false; loadingMessage="Unable to login!"}
                     )
             }
         }
     }
 }
 @Composable
-fun AnimatedDotsText(baseText: String, color: androidx.compose.ui.graphics.Color, onFinishLoading: () -> Unit) {
+fun AnimatedDotsText(baseText: String, color: Color, onFinishLoading: () -> Unit) {
     var dotCount by remember { mutableIntStateOf(0) }
     LaunchedEffect(Unit) {
-        while (dotCount<10) {
-            dotCount = (dotCount + 1)
-            kotlinx.coroutines.delay(500)
+        while (dotCount<20) {
+            dotCount++;
+            delay(500)
         }
         onFinishLoading()
     }
@@ -85,10 +136,8 @@ fun AnimatedDotsText(baseText: String, color: androidx.compose.ui.graphics.Color
     val dots = ".".repeat(dotCount%4)
     Text(text = "$baseText$dots", color = color)
 }
-
-
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
+fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: (login : String, password : String) -> Unit, message : String) {
     var login by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val logo = painterResource(R.drawable.logo)
@@ -97,15 +146,15 @@ fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
         modifier = modifier,
     ) {
         Column(
-            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
-            verticalArrangement = androidx.compose.foundation.layout.Arrangement.Top
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
         ) {
             Image(
                 painter = logo,
                 contentDescription = null,
 
             )
-            androidx.compose.material3.OutlinedTextField(
+            OutlinedTextField(
                 value = login,
                 onValueChange = {login=it},
                 label = { Text("Username") },
@@ -121,11 +170,11 @@ fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
                 unfocusedTextColor = colorResource(id = R.color.hackerBlue)
             )
             )
-            androidx.compose.material3.OutlinedTextField(
+            OutlinedTextField(
                 value = password,
                 onValueChange = {password=it},
                 label = { Text("Password") },
-                visualTransformation = androidx.compose.ui.text.input.PasswordVisualTransformation(),
+                visualTransformation = PasswordVisualTransformation(),
                 colors =  TextFieldDefaults.colors(
                     focusedContainerColor = colorResource(id = android.R.color.transparent),
                     unfocusedContainerColor = colorResource(id = android.R.color.transparent),
@@ -138,10 +187,10 @@ fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
                     unfocusedTextColor = colorResource(id = R.color.hackerBlue)
                 )
             )
-            androidx.compose.material3.Button(
+            if(message != "") Text(message, color = colorResource(id = R.color.hackerRed))
+            Button(
                 onClick = {
-                    password = ""
-                    onLoginClicked()
+                    onLoginClicked(login, password)
                 },
                 border = BorderStroke(
                     width = 2.dp,
@@ -152,7 +201,7 @@ fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
                     .padding(top = 16.dp)
                     .background(
                         color = colorResource(id = android.R.color.transparent),
-                        shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp)
+                        shape = RoundedCornerShape(4.dp)
                     ),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = colorResource(id = android.R.color.transparent),
